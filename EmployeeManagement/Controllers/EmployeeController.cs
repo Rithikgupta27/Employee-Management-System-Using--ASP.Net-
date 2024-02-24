da2c3ba2667243dfb7fdb97bae242ca7c8e5eb99
Rithikgupta27 <rithikg2700@gmail.com>
2024-02-24 18:59:26 +0530

    employee management system

---
+++ EmployeeManagement/Controllers/EmployeeController.cs
@@ -0,0 +1,92 @@
+﻿using EmployeeManagement.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EmployeeManagement.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        // GET: EmployeeController
+        public ActionResult Index()
+        {
+            List<Employee> list = Employee.GetAll();
+            return View(list);
+        }
+
+        // GET: EmployeeController/Details/5
+        public ActionResult Details(int id)
+        {
+            Employee emp = Employee.GetDetails(id);
+            return View(emp);
+        }
+
+        // GET: EmployeeController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: EmployeeController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection,Employee obj)
+        {
+            try
+            {
+                Employee.Insert(obj);
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: EmployeeController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            Employee emp = Employee.GetDetails(id);
+            return View(emp);
+        }
+
+        // POST: EmployeeController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id,Employee obj, IFormCollection collection)
+        {
+            try
+            {
+                Employee.Update(obj);
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+
+                return View();
+            }
+        }
+
+        // GET: EmployeeController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            Employee emp = Employee.GetDetails(id);
+            return View(emp);
+        }
+
+        // POST: EmployeeController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                Employee.Delete(id);
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ EmployeeManagement/Models/Employee.cs
@@ -0,0 +1,139 @@
+﻿using Microsoft.Data.SqlClient;
+using System.ComponentModel.DataAnnotations;
+using System.Data;
+namespace EmployeeManagement.Models
+{
+    public class Employee
+    {
+        [Key]
+        public int EmpId { get; set; }
+
+        [Required(ErrorMessage ="Name not given")]
+        [StringLength(50)]
+        public string EmpName { get; set; }
+
+        [Required(ErrorMessage = "City not given")]
+        [StringLength(20)]
+        public string City { get; set; }
+
+        [Required(ErrorMessage = "Address not given")]
+        [StringLength(100)]
+        public string Address { get; set; }
+
+         public static void Insert(Employee employee)
+        {
+             SqlConnection conn = new SqlConnection();
+             conn.ConnectionString = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=EmployeeDB;Integrated Security=True;";
+             try
+            {
+                conn.Open();
+                SqlCommand cmd = new SqlCommand();
+                cmd.Connection = conn;
+                cmd.CommandType = System.Data.CommandType.Text;
+                cmd.CommandText = "insert into Tbl_Emp values(@EmpName,@City,@Address)";
+                //cmd.Parameters.AddWithValue("@EmpId", employee.EmpId);
+                cmd.Parameters.AddWithValue("@EmpName", employee.EmpName);
+                cmd.Parameters.AddWithValue("@City", employee.City);
+                cmd.Parameters.AddWithValue("@Address", employee.Address);
+                cmd.ExecuteNonQuery();
+            }
+            catch (Exception ex) { Console.WriteLine(ex.Message); }
+            finally { conn.Close(); }
+        }
+        public static void Update(Employee obj)
+        {
+            SqlConnection conn = new SqlConnection();
+            conn.ConnectionString = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=EmployeeDB;Integrated Security=True;";
+            try
+            {
+                conn.Open();
+                SqlCommand cmd = new SqlCommand();
+                cmd.Connection = conn;
+                cmd.CommandType = System.Data.CommandType.Text;
+                cmd.CommandText = "update Tbl_Emp set EmpName = @EmpName,City = @City,Address = @Address where EmpId = @id";
+                cmd.Parameters.AddWithValue("@EmpId", obj.EmpId);
+                cmd.Parameters.AddWithValue("@EmpName", obj.EmpName);
+                cmd.Parameters.AddWithValue("@City", obj.City);
+                cmd.Parameters.AddWithValue("@Address", obj.Address);
+                cmd.ExecuteNonQuery();
+               
+            }
+            catch (Exception ex) { Console.WriteLine(ex.Message); }
+            finally { conn.Close(); }
+        }
+        public static List<Employee> GetAll()
+        {
+           List< Employee> emp = new List<Employee>();
+            SqlConnection conn = new SqlConnection();
+            conn.ConnectionString = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=EmployeeDB;Integrated Security=True;";
+            try
+            {
+                conn.Open();
+                SqlCommand cmd = new SqlCommand();
+                cmd.Connection = conn;
+                cmd.CommandType = System.Data.CommandType.Text;
+                cmd.CommandText = "select * from Tbl_Emp";
+                SqlDataReader reader = cmd.ExecuteReader();
+                while (reader.Read())
+                {
+                    emp.Add(new Employee { EmpId = reader.GetInt32(0), EmpName = reader.GetString(1), City = reader.GetString(2), Address = reader.GetString(3)});
+                }
+
+            }
+
+            catch (Exception ex) { Console.WriteLine(ex.Message); }
+            finally { conn.Close(); }
+            return emp;
+         }
+          public static void Delete(int empNo) 
+          {
+            List<Employee> emp = new List<Employee>();
+            SqlConnection conn = new SqlConnection();
+            conn.ConnectionString = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=EmployeeDB;Integrated Security=True;";
+            try
+            {
+                conn.Open();
+                SqlCommand cmd = new SqlCommand();
+                cmd.Connection = conn;
+                cmd.CommandType = System.Data.CommandType.Text;
+                cmd.CommandText = "delete from Tbl_Emp where EmpId = @empNo";
+                cmd.Parameters.AddWithValue("@EmpId", empNo);
+                cmd.ExecuteNonQuery();
+
+            }
+            catch (Exception ex) { Console.WriteLine(ex.Message); }
+            finally { conn.Close(); }
+        }
+        public static Employee GetDetails(int empNo) {
+          Employee employee = new Employee();
+            SqlConnection conn = new SqlConnection();
+            conn.ConnectionString = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=EmployeeDB;Integrated Security=True;";
+            try
+            {
+                conn.Open();
+                SqlCommand cmd = new SqlCommand();
+                cmd.Connection = conn;
+                cmd.Parameters.AddWithValue("@EmpId", empNo);
+                cmd.CommandType = System.Data.CommandType.Text;
+                cmd.CommandText = "select * from Tbl_Emp where EmpId = @EmpId";
+                SqlDataReader reader = cmd.ExecuteReader();
+                if(reader.Read())
+                {
+                    employee.EmpId = reader.GetInt32("EmpId");
+                    employee.EmpName = reader.GetString("EmpName");
+                    employee.City = reader.GetString("City");
+                    employee.Address = reader.GetString("Address");
+                cmd.ExecuteScalar();
+                }
+                else
+                {
+                    Console.WriteLine("not found");
+                }
+            }
+            catch (Exception ex) { Console.WriteLine(ex.Message); }
+            finally { conn.Close(); }
+            return employee;
+        }
+    }
+    
+}
